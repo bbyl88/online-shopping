20ea91bba4987d2e3c2cc0046ef06c6de8a6fe44
Evelyn <bbyl88@gmail.com>
2018-06-03 22:19:11 +0800

    Project setup completed successfully

---
+++ onlinestorebackend/src/main/java/sg/ep/onlinestorebackend/App.java
@@ -0,0 +1,13 @@
+package sg.ep.onlinestorebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
